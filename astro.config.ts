660b10f3695b6f2c14c993f2c46ffb17724b6648
Marc Vila <marc@mhou.es>
2024-07-11 18:41:48 +0200

    new footer

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
+	//base: '/bc/desmintiendomitos/',
 	devToolbar: { enabled: false },
 	integrations: [tailwind({ applyBaseStyles: false })],
 })
